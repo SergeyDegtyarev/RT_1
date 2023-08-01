c5c2335c4b52ae82ccb10f5303499af776526c0c
Serg <sealek@gmail.com>
2023-08-01 18:36:09 +0400

    2

--- HW/src/Main.java
+++ HW/src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static int getNumberOfMaxParam(int a, int b, int c) {
+        if ((a>=b)&&(a>=c)) return 1;
+        if ((b>=a)&&(b>=c)) return 2;
+        return 3;
+
+
+
+
+        // write a body here
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello adsasdfadf fasdfasdf        world!");
+        String a = new String("1");
+        String c = new String("1");
+        System.out.println(a == c);
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        String b = scanner.next();
+        switch (b){
+            case ("gryffindor"): System.out.println("bravery"); break;
+            case ("hufflepuff"): System.out.println("loyalty"); break;
+            case ("slytherin"): System.out.println("cunning"); break;
+            case ("ravenclaw"): System.out.println("intellect"); break;
+            default: System.out.println("not a valid house"); break;
+        }
+
+
+
+
     }
 }
