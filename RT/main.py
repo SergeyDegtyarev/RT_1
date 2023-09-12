69e8fa38801e3c3476bd0a6fac7c6c0bd95c82fc
Serg <sealek@gmail.com>
2023-09-12 14:16:25 +0400

    4

--- RT/main.py
+++ RT/main.py
@@ -11,7 +11,7 @@ import matplotlib as mpl
 mpl.rcParams['axes.formatter.use_locale'] = True
 
 
-print("Enter origin of ray: ")
+#print("Enter origin of ray: ")
 a1, a2, a3 = [float(s) for s in input().split()]
 rad_vec_ray = [a1, a2, a3]
 print("Enter direction vector of ray: ")
